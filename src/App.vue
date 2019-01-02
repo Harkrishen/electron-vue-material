a2fe17ac54aea2f7840f03ca90430365325e7495
Harkrishen <zdac105@live.rhul.ac.uk>
2019-01-02 12:34:02 +0000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+<!--
 <template>
   <div id="app">
     <div id="nav">
@@ -29,3 +30,101 @@
   color: #42b983;
 }
 </style>
+-->
+
+<template>
+  <div>
+    <md-app md-waterfall md-mode="flexible">
+      <md-app-toolbar class="md-large md-primary">
+        <div class="md-toolbar-row">
+          <div class="md-toolbar-section-start">
+            <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+              <md-icon>menu</md-icon>
+            </md-button>
+          </div>
+
+          <div class="md-toolbar-section-end">
+            <md-button class="md-icon-button">
+              <md-icon>more_vert</md-icon>
+            </md-button>
+          </div>
+        </div>
+
+        <div class="md-toolbar-row md-toolbar-offset">
+          <span class="md-display-1">Electron Vue Material</span>
+        </div>
+      </md-app-toolbar>
+
+      <md-app-drawer :md-active.sync="menuVisible">
+        <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
+
+        <md-list>
+          <md-list-item>
+            <md-icon>move_to_inbox</md-icon>
+            <span class="md-list-item-text">
+                <router-link to="/">Home</router-link>
+              </span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>send</md-icon>
+            <span class="md-list-item-text">
+                <router-link to="/about">About</router-link>
+              </span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>delete</md-icon>
+            <span class="md-list-item-text">Trash</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>error</md-icon>
+            <span class="md-list-item-text">Spam</span>
+          </md-list-item>
+        </md-list>
+      </md-app-drawer>
+
+      <md-app-content>
+        <div id="app">
+          <router-view/>
+        </div>
+      </md-app-content>
+    </md-app>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .md-app {
+    max-height: 540px;
+    //border: 1px solid rgba(#000, .12);
+  }
+
+  .md-app-toolbar {
+    height: 196px;
+    background-color: #c10000;
+  }
+
+  // Demo purposes only
+  .md-drawer {
+    width: 230px;
+    max-width: calc(100vw - 125px);
+    background-color: white;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+</style>
+
+<script>
+  export default {
+    data: () => ({
+      menuVisible: false
+    })
+  }
+</script>
